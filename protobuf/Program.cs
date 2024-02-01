2afc24a9f9ae28622faa466b73bf749243048cb2
FilipposKontoes <fil13kon@gmail.com>
2024-02-01 18:03:40 +0200

    Add protobuf functionality and dependencies

--- protobuf/Program.cs
+++ protobuf/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Protobuf.Test;
+using static Grpc.Protobuf.Test.Employee.Types;
+
+System.Console.WriteLine("Welecome to protobuf test!");
+var employee = new Employee();
+employee.FirstName = "Filippos";
+employee.LastName = "Kon";
+employee.IsRetired = false;
+var birthdate = new DateTime(1995, 8, 17);
+birthdate = DateTime.SpecifyKind(birthdate, DateTimeKind.Utc);
+employee.BirthDate = Timestamp.FromDateTime(birthdate);
+employee.MaritalStatus = MaritalStatus.Single;
+employee.PreviousEmployers.Add("Microsoft");
+employee.PreviousEmployers.Add("Google");
+
+Console.WriteLine("Test completed");
--- protobuf/obj/Debug/net8.0/protobuf.AssemblyInfo.cs
+++ protobuf/obj/Debug/net8.0/protobuf.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("protobuf")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7da8fd3a209518fe29c751296db2127ea64ecf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("protobuf")]
 [assembly: System.Reflection.AssemblyTitleAttribute("protobuf")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
