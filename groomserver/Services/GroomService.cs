1faca33b55b21621bb14cfacbc74ba1c750cdcf8
FilipposKontoes <fil13kon@gmail.com>
2024-02-15 17:21:56 +0200

    Add StartChat method to GroomService

--- groomserver/Services/GroomService.cs
+++ groomserver/Services/GroomService.cs
@@ -55,4 +55,83 @@ public class GroomService : Groom.GroomBase
 
     }
 
+    public override async Task StartChat(IAsyncStreamReader<ChatMessage> incomingStream, IServerStreamWriter<ChatMessage> outgoingStream, ServerCallContext context)
+    {
+
+        // Wait for the first message to get the user name
+        while (!await incomingStream.MoveNext())
+        {
+            await Task.Delay(100);
+        }
+
+        string userName = incomingStream.Current.User;
+        string room = incomingStream.Current.Room;
+        Console.WriteLine($"User {userName} connected to room {incomingStream.Current.Room}");
+
+        // TEST TEST TEST TEST - TO USE ONLY WHEN TESTING WITH BLOOMRPC
+        UsersQueues.CreateUserQueue(room, userName);
+        // END TEST END TEST END TEST
+
+        // Get messages from the user
+        var reqTask = Task.Run(async () =>
+        {
+            while (await incomingStream.MoveNext())
+            {
+                Console.WriteLine($"Message received: {incomingStream.Current.Content}");
+                UsersQueues.AddMessageToRoom(ConvertToReceivedMessage(incomingStream.Current), incomingStream.Current.Room);
+            }
+        });
+
+
+        // Check for messages to send to the user
+        var respTask = Task.Run(async () =>
+        {
+            while (true)
+            {
+                var userMsg = UsersQueues.GetMessageForUser(userName);
+                if (userMsg != null)
+                {
+                    var userMessage = ConvertToChatMessage(userMsg, room);
+                    await outgoingStream.WriteAsync(userMessage);
+                }
+                if (MessagesQueue.GetMessagesCount() > 0)
+                {
+                    var news = MessagesQueue.GetNextMessage();
+                    var newsMessage = ConvertToChatMessage(news, room);
+                    await outgoingStream.WriteAsync(newsMessage);
+                }
+
+                await Task.Delay(200);
+            }
+        });
+
+        // Keep the method running
+        while (true)
+        {
+            await Task.Delay(10000);
+        }
+    }
+
+
+    private ReceivedMessage ConvertToReceivedMessage(ChatMessage chatMsg)
+    {
+        var rcMsg = new ReceivedMessage();
+        rcMsg.Content = chatMsg.Content;
+        rcMsg.MsgTime = chatMsg.MsgTime;
+        rcMsg.User = chatMsg.User;
+
+        return rcMsg;
+    }
+
+    private ChatMessage ConvertToChatMessage(ReceivedMessage rcMsg, string room)
+    {
+        var chatMsg = new ChatMessage();
+        chatMsg.Content = rcMsg.Content;
+        chatMsg.MsgTime = rcMsg.MsgTime;
+        chatMsg.User = rcMsg.User;
+        chatMsg.Room = room;
+
+        return chatMsg;
+    }
+
 }
